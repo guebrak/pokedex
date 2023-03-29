7955411c7e4ed23680858b658c03eb78b7c7de53
Gabriel Pilaud <gabriel.pilaud@hotmail.fr>
2023-03-29 14:59:46 +0200

    exo 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,25 @@
 function PokemonCard() {
+    // const pokemon = pokemonList[0]
+
     return (
       <figure className="card">
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-          className="card-img"
-        />
-        <figcaption>bulbasaur</figcaption>
+        {pokemonList .imgSrc ? <img src={pokemonList[0].imgSrc} alt={pokemonList[0].name}
+        className="card-img" /> : <p>???</p>}
+
+        <figcaption>{pokemonList [0].name}</figcaption>
       </figure>
     );
-  }
+}
   
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   export default PokemonCard;
