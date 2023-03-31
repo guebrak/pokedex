dcde9816d44e6b88ab437c845e62726600e6029c
Gabriel Pilaud <gabriel.pilaud@hotmail.fr>
2023-03-31 11:49:29 +0200

    state.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -9,18 +10,46 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClickNext = () => {
+    setCount(count + 1)
+  }
+  const handleClickPrev = () => {
+    setCount(count - 1)
+  }
+
   
 
   return (
       <div>
-       <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc}/>
+       <PokemonCard name={pokemonList[count].name} imgSrc={pokemonList[count].imgSrc}/>
+       <button onClick={handleClickPrev}>prev</button>
+       <button onClick={handleClickNext}>next</button>
     </div>
   )
 }
+
+
 
 export default App
